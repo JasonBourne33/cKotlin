a315b71fc94efee852ab7b9180728e6e4fa07f6c
chaos <1065807387@qq.com>
2022-11-25 18:10:04 +0800

    11.25

--- app/src/main/java/com/example/ckotlin/BaseActivity.kt
+++ app/src/main/java/com/example/ckotlin/BaseActivity.kt
@@ -9,6 +9,7 @@ open class BaseActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("BaseActivity",javaClass.simpleName)
+        ActivityBox.addActivity(this)
     }
 
 }
